aa4485d69b2f55d18efc55929fede0df6ab435e0
Skull33 <alonzo.cuela@ucsp.edu.pe>
2025-08-20 14:01:27 -0500

    AED_part2 v0.01

--- BuscaMinas/BuscaMinas/BuscaMinas.cpp
+++ BuscaMinas/BuscaMinas/BuscaMinas.cpp
@@ -21,7 +21,7 @@ int main()
 		cout << endl;
 	}
 
-	cout << "ignrese el numero de minas para detectar: ";
+	cout << "ingrese el numero de minas para detectar: ";
 	cin >> minas;
 	cout << "se han detectado " << minas << " en el campo, tenga cuidado" << endl;
 
@@ -33,17 +33,45 @@ int main()
 		*(*(campo_minado + f) + c) = -1;
 	}
 
-	//calcular la cantidad de minar en cada espacio
+	//calcular la cantidad de minas en cada espacio
 	for (int (*filas)[10] = campo_minado; filas < campo_minado + 10; filas++)
 	{
 		for (int* columnas = *filas; columnas < *filas + 10; columnas++)
 		{
+			int contador = 0;
+			//evitar chancar el numero detector encima de la mina
 			if (*columnas == -1)
 			{
 				continue;
 			}
+			//detectar las minas
+			else
+			{
+				//recorremos el campo para ennumerar los espacios
+				int fila_actual = filas - campo_minado;
+				int columna_actual = columnas - *filas;
+				//los numeros detectores oslo recorren en una matriz de 3*3
+				for (int fila_deteccion = fila_actual - 1; fila_deteccion < fila_actual + 2; fila_deteccion++)
+				{
+					if (fila_deteccion < 0 || fila_deteccion >= 10)
+					{
+						continue;
+					}
+					for (int columna_deteccion = columna_actual - 1; columna_deteccion < columna_actual + 2; columna_deteccion++)
+					{
+						if (columna_deteccion < 0 || columna_deteccion >= 10)
+						{
+							continue;
+						}
+						if (campo_minado[fila_deteccion][columna_deteccion] == -1)
+						{
+							contador++;
+						}
+					}
+				}
+				*columnas = contador;
+			}
 		}
-		cout << endl;
 	}
 
 	//dibujar el campo minado
