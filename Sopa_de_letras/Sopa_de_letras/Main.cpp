1d7f8f27f52f5a9f3b9e284891774e3fa69d36e3
Alonzo Cuela <sku11stds@gmail.com>
2025-09-05 22:53:11 -0500

    AED_part2 v0.06

--- Sopa_de_letras/Sopa_de_letras/Main.cpp
+++ Sopa_de_letras/Sopa_de_letras/Main.cpp
@@ -37,13 +37,78 @@ int main()
 	cout << "la palabra que usted desea buscar es: " << palabra_array << " con " << nro_elementos << " elementos" << endl;
 	int contador_de_palabras = 0;
 	//detector de palabras
-	//recorriendo a la derecha
+	//recorriendo de derecha a izquierda
 	for (char(*f)[10] = sopa_de_letras; f < sopa_de_letras + 10; f++)
 	{
 		//recorrer a la derecha
 		for (char* col_derecha = *f; col_derecha < *f + 10; col_derecha++)
 		{
 			if (*col_derecha == *deteccion_letras)
+			{
+				deteccion_letras++;
+				if (deteccion_letras - palabra_array == longitud_original)
+				{
+					contador_de_palabras++;
+					deteccion_letras = palabra_array;
+				}
+			}
+			else
+			{
+				deteccion_letras = palabra_array;
+				continue;
+			}
+		}
+		//recorrido a la izquierda
+		deteccion_letras = palabra_array;
+		for (char* col_izquierda = *f + 9; col_izquierda >= *f; col_izquierda--)
+		{
+			if (*col_izquierda == *deteccion_letras)
+			{
+				deteccion_letras++;
+				if (deteccion_letras - palabra_array == longitud_original)
+				{
+					contador_de_palabras++;
+					deteccion_letras = palabra_array;
+				}
+			}
+			else
+			{
+				deteccion_letras = palabra_array;
+				continue;
+			}
+		}
+	}
+
+	//recorriendo de arriba a abajo
+	for (int col = 0; col < 10; col++)
+	{
+		//recorrer de arriba a abajo
+		deteccion_letras = palabra_array;
+		for (char(*fil)[10] = sopa_de_letras; fil < sopa_de_letras + 10; fil++)
+		{
+			char letra = *(*(fil)+col);
+			if (letra == *deteccion_letras)
+			{
+				deteccion_letras++;
+				if (deteccion_letras - palabra_array == longitud_original)
+				{
+					contador_de_palabras++;
+					deteccion_letras = palabra_array;
+				}
+			}
+			else
+			{
+				deteccion_letras = palabra_array;
+				continue;
+			}
+		}
+
+		//recorrer de abajo a arriba
+		deteccion_letras = palabra_array;
+		for (char(*fil)[10] = sopa_de_letras + 9; fil >= sopa_de_letras; fil--)
+		{
+			char letra = *(*(fil)+col);
+			if (letra == *deteccion_letras)
 			{
 				deteccion_letras++;
 				if (deteccion_letras - palabra_array == longitud_original)
