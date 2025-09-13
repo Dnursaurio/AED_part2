4cca55a69ad1b187863fb7a4518206ebeed7929b
Skull33 <alonzo.cuela@ucsp.edu.pe>
2025-09-13 01:49:54 -0500

    AED_part2 v0.09

--- Laberinto/Laberinto/Main.cpp
+++ Laberinto/Laberinto/Main.cpp
@@ -2,44 +2,81 @@
 
 using namespace std;
 
+char laberinto[10][10] = {
+    {'A','*',' ',' ',' ',' ',' ',' ',' ','*'},
+    {' ','*',' ',' ',' ',' ','*','*',' ','*'},
+    {' ','*',' ','*','*',' ','*','*','*','*'},
+    {' ','*',' ','*',' ',' ','*',' ',' ',' '},
+    {' ',' ',' ','*',' ',' ',' ',' ','*','*'},
+    {' ','*','*','*','*','*',' ',' ','*',' '},
+    {' ',' ',' ',' ','*',' ',' ',' ',' ',' '},
+    {' ','*','*',' ','*',' ',' ','*','*','*'},
+    {'*','*',' ',' ','*',' ',' ','*',' ',' '},
+    {' ',' ',' ','*','*',' ',' ',' ',' ','B'}
+};
+
+bool recorrer(char (*fila_actual)[10], char* columna_actual) {
+    if (*columna_actual == 'B') return true;
+
+    if (*columna_actual != 'A') {
+        if (*columna_actual != ' ') return false;
+        *columna_actual = '+';
+    }
+
+    int fila_detectada = fila_actual - laberinto;
+    int columna_detectada = columna_actual - *fila_actual;
+
+    int mov[4][2] = { {1,0}, {-1,0}, {0,1}, {0,-1} };
+
+    for (int i = 0; i < 4; i++) {
+        int siguiente_fila = fila_detectada + mov[i][0];
+        int siguiente_columna = columna_detectada + mov[i][1];
+
+        if (siguiente_fila < 0 || siguiente_fila >= 10 || siguiente_columna < 0 || siguiente_columna >= 10)
+            continue;
+
+        if (recorrer(laberinto + siguiente_fila, *(laberinto + siguiente_fila) + siguiente_columna)) {
+            return true;
+        }
+    }
+
+    if (*columna_actual == '+') *columna_actual = ' ';
+    return false;
+}
+
 int main()
 {
-	cout << "---------------------------Laberinto A_B---------------------------" << endl;
-	char laberinto[10][10] = {
-		{'A','*',' ',' ',' ',' ',' ',' ',' ','*'},
-		{' ','*',' ',' ',' ',' ','*','*',' ','*'},
-		{' ','*',' ','*','*',' ','*','*','*','*'},
-		{' ','*',' ','*',' ',' ','*',' ',' ',' '},
-		{' ',' ',' ','*',' ',' ',' ',' ','*','*'},
-		{' ','*','*','*','*','*',' ',' ','*',' '},
-		{' ',' ',' ',' ','*',' ',' ',' ',' ',' '},
-		{' ','*','*',' ','*',' ',' ','*','*','*'},
-		{'*','*',' ',' ','*',' ',' ','*',' ',' '},
-		{' ',' ',' ','*','*',' ',' ',' ',' ','B'}
-	};
+    cout << "---------------------------Laberinto A_B---------------------------" << endl;
 
-	//Graficamos el laberinto
-	for (char(*filas)[10] = laberinto; filas < laberinto + 10; filas++)
-	{
-		for (char* columnas = *filas; columnas < *filas + 10; columnas++)
-		{
-			cout << *columnas << " ";
-		}
-		cout <<"|" << endl;
-	}
-	cout << "---------------------" << endl;
-	//recorriendo el array pero con una malla de 3*3 paea detectar espacios vacios
-	for (char(*filas)[10] = laberinto; filas < laberinto + 10; filas++)
-	{
-		for (char* columnas = *filas; columnas < *filas + 10; columnas++)
-		{
-			int caminos_posibles = 0;
-			//aqui que detecte el 3*3a
-			int fila_actual =filas - laberinto;
-			int columnas_actual = columnas - *filas;
+    for (char(*filas)[10] = laberinto; filas < laberinto + 10; filas++) {
+        for (char* columnas = *filas; columnas < *filas + 10; columnas++) {
+            cout << *columnas << " ";
+        }
+        cout << "|" << endl;
+    }
 
+    cout << "---------------------" << endl;
 
-		}
-	}
-	cout << "-------------------------------------------------------------------" << endl;
+    char(*fila_actual)[10] = laberinto;
+    char* columna_actual = *fila_actual;
+    for (char(*filas)[10] = laberinto; filas < laberinto + 10; filas++) {
+        for (char* columnas = *filas; columnas < *filas + 10; columnas++) {
+            if (*columnas == 'A') {
+                fila_actual = filas;
+                columna_actual = columnas;
+            }
+        }
+    }
+
+    recorrer(fila_actual, columna_actual);
+
+    for (char(*filas)[10] = laberinto; filas < laberinto + 10; filas++) {
+        for (char* columnas = *filas; columnas < *filas + 10; columnas++) {
+            cout << *columnas << " ";
+        }
+        cout << "|" << endl;
+    }
+    cout << "---------------------" << endl;
+
+    cout << "-------------------------------------------------------------------" << endl;
 }
