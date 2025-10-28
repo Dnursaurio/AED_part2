f0dd7c444e7d8791bc0cf3194a1e5b8516b64a25
Alonzo Cuela <sku11stds@gmail.com>
2025-10-27 19:03:54 -0500

    AED_part2 v0.26

--- Graficacion/Graficacion/Main.cpp
+++ Graficacion/Graficacion/Main.cpp
@@ -1,55 +1,62 @@
+#include <vector>
 #include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <iostream>
 
 struct Nodo
 {
-	Nodo(int val)
-	{
-		//eso aun es en consola
-		v = val;
-		izq = NULL;
-		der = NULL;
-	}
-	int v;
-	Nodo* izq;
-	Nodo* der;
+    Nodo(int val)
+    {
+        v = val;
+        izq = NULL;
+        der = NULL;
+    }
+    int v;
+    Nodo* izq;
+    Nodo* der;
 };
 
-class arbolBin
+void InOrder(Nodo* n)
 {
-public:
-	arbolBin()
-	{
-		raiz = nullptr;
-	}
-	bool Insert(Nodo**& ptr,int valor)
-	{
-		for (ptr = &raiz; *ptr && (*ptr)->v != valor;);
-	}
-private:
-	Nodo* raiz;
-};
+    if (!n)
+    {
+        return;
+    }
+    InOrder(n->izq);
+    std::cout << n->v << " ";
+    InOrder(n->der);
+}
+
+Nodo* CrearArbol(int* ini, int* fin)
+{
+    std::vector<Nodo*>v;
+    for (; ini < fin; ini++)
+    {
+        Nodo* n = new Nodo(*ini);
+        v.push_back(n);
+    }
+    while (v.size() > 1)
+    {
+        int suma = v[0]->v + v[1]->v;
+        Nodo* n2 = new Nodo(suma);
+        n2->izq = v[0];
+        n2->der = v[1];
+        v.erase(v.begin());
+        v.erase(v.begin());
+        v.push_back(n2);
+    }
+
+    Nodo* raiz = v.front();
+    InOrder(raiz);
+    return raiz;
+}
 
 int main()
-{
-	SDL_Window* ventana = SDL_CreateWindow("Graficacion de arbol", 800, 600, 0);
-	SDL_Renderer* render = SDL_CreateRenderer(ventana, NULL);
-	SDL_Event evento;
+{   
+    int arr[16] = { 2,4,7,9,10,15,3,25,38,74,1,5,10,13,11,35 };
+    int* ini = arr;
+    int* fin = arr + 16;
+    CrearArbol(ini, fin);
 
-	SDL_SetRenderDrawColor(render, 50, 50, 50, 255);
-	SDL_RenderClear(render);
-	SDL_RenderPresent(render);
-	bool ventana_abierta = 1;
-	while (ventana_abierta)
-	{
-		SDL_PollEvent(&evento);
-		if (evento.type == SDL_EVENT_QUIT)
-		{
-			ventana_abierta = 0;
-		}
-	}
-	
-	SDL_DestroyRenderer(render);
-	SDL_DestroyWindow(ventana);
-
-	return 0;
+    return 0;
 }
