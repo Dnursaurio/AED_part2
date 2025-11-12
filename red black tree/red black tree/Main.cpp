b7aac22369d60fbeae62758d4e7060367ecb34d1
Skull33 <145301098+Skull33@users.noreply.github.com>
2025-11-12 13:51:38 -0500

    AED_part2 v0.32

--- red black tree/red black tree/Main.cpp
+++ red black tree/red black tree/Main.cpp
@@ -5,23 +5,59 @@ using namespace std;
 
 struct Nodo
 {
-	Nodo(int v, string c)
+	Nodo(int v)
 	{
 		valor = v;
-		color = c;
+		color = true; // color "rojo"
+		padre = nullptr;
 		izq = nullptr;
 		der = nullptr;
 	}
 
 	int valor;
-	string color;
+	bool color;
+	Nodo* padre;
 	Nodo* izq;
 	Nodo* der;
 };
 
 class RBtree
 {
+public:
+	RBtree()
+	{
+		root = nullptr;
+	}
 
+	bool Find(Nodo** ptr, int v)
+	{
+		ptr = &root;
+		while (*ptr && (*ptr)->valor != v)
+		{
+			if ((*ptr)->valor < v)
+			{
+				ptr = &((*ptr)->izq);
+			}
+			else
+			{
+				ptr = &((*ptr)->izq);
+			}
+		}
+		return *ptr != nullptr;
+	}
+
+	bool Insert(int val)
+	{
+
+	}
+
+	bool Remove(int val)
+	{
+
+	}
+
+private:
+	Nodo* root;
 };
 
 int main()
