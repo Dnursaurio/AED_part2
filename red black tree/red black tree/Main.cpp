9778f367e46b5b4d8425b3a43c841f453643f592
Alonzo Cuela <sku11stds@gmail.com>
2025-11-12 22:27:18 -0500

    AED_part2 v0.33

--- red black tree/red black tree/Main.cpp
+++ red black tree/red black tree/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stack>
 
 using namespace std;
 
@@ -29,35 +30,144 @@ public:
 		root = nullptr;
 	}
 
-	bool Find(Nodo** ptr, int v)
+	bool Find(Nodo**& ptr, int v)
 	{
+		while (!s.empty())
+		{
+			s.pop();
+		}
+
 		ptr = &root;
 		while (*ptr && (*ptr)->valor != v)
 		{
+			s.push(*ptr);
 			if ((*ptr)->valor < v)
 			{
 				ptr = &((*ptr)->izq);
 			}
 			else
 			{
-				ptr = &((*ptr)->izq);
+				ptr = &((*ptr)->der);
 			}
 		}
 		return *ptr != nullptr;
 	}
 
+	Nodo* Tio(Nodo* papa, Nodo**ptr)
+	{
+		Nodo* tio = nullptr;
+		if (!papa)
+		{
+			return;
+		}
+		else
+		{
+			if (papa->izq == (*ptr))
+			{
+				tio = papa->der;
+			}
+			else
+			{
+				tio = papa->izq;
+			}
+		}
+		return tio;
+	}
+
 	bool Insert(int val)
 	{
+		Nodo** ptr;
+		if (Find(ptr, val))
+		{
+			return 0;
+		}
+		else
+		{
+			*ptr = new Nodo(val);
+			s.pop();
+			(*ptr)->padre = s.empty() ? nullptr : s.top();
+			Nodo* papa = (*ptr)->padre;
 
+			if (*ptr == root)
+			{
+				(*ptr)->color = false;
+			}
+			if ((*ptr)->color == true)
+			{
+				if ((*ptr)->izq && (*ptr)->izq->color == true)
+				{
+					Nodo* tio = Tio(papa, ptr);
+					if (tio->color == true)
+					{
+						papa->color = true;
+						tio->color = false;
+						(*ptr)->color = false;
+					}
+					if (tio->color == false)
+					{
+
+					}
+				}
+			}
+
+			return 1;
+		}
+	}
+
+	Nodo** Reemplazo(Nodo** q)
+	{
+		Nodo** ptr = q;
+		if (rand() % 2 == 0)
+		{
+			q = &(*q)->izq;
+			while ((*q)->der)
+			{
+				q = &((*q)->der);
+			}
+		}
+		else
+		{
+			q = &(*q)->der;
+			while ((*q)->izq)
+			{
+				q = &((*q)->izq);
+			}
+		}
+		return q;
 	}
 
 	bool Remove(int val)
 	{
-
+		Nodo** ptr;
+		if (!Find(ptr, val))
+		{
+			return 0;
+		}
+		else
+		{
+			if ((*ptr)->izq && (*ptr)->der)
+			{
+				Nodo** q = Reemplazo(ptr);
+				(*ptr)->valor = (*q)->valor;
+				ptr = q;
+			}
+			Nodo* temp = *ptr;
+			if ((*ptr)->izq)
+			{
+				*ptr = temp->izq;
+			}
+			else
+			{
+				*ptr = temp->der;
+			}
+			delete temp;
+			return 1;
+		}
 	}
 
 private:
 	Nodo* root;
+	stack<Nodo*>s;
 };
 
 int main()
